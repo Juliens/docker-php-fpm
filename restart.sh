b2c3f989951be68b3e8c46a69d01abb64fe21190
SALLEYRON Julien <julien.salleyron@gmail.com>
2015-03-08 15:15:10 +0100

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-kill -9 $(ps -ef | grep php-fpm | grep -v tail | awk '{print $2}')
-kill -9 $(ps -ef | grep httpd | grep -v tail | awk '{print $2}')
+kill -15 $(cat /var/run/httpd/httpd.pid /var/run/php-fpm/php-fpm.pid)
 export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
 httpd
 php-fpm
